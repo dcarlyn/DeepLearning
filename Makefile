d1ef2984e49796b84103801e3ed70daf867b0d00
David Carlyn <davidecarlyn@gmail.com>
2019-03-22 02:23:25 -0700

    yee

---
+++ Makefile
@@ -0,0 +1,8 @@
+learn: gradientDescent.cpp
+	clang++ -std=c++11 gradientDescent.cpp
+
+run:
+	./a.out
+
+clean:
+	rm a.out
--- gradientDescent.cpp
+++ gradientDescent.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 struct ThreeDPoint {
 	double x;
@@ -39,7 +40,6 @@ int main() {
 }
 
 void setLearningRate( double& learning_rate ) {
-	std::cout << "Set Learning Rate: ";
 	std::cin >> learning_rate;
 }
 
@@ -50,26 +50,18 @@ std::vector<ThreeDPoint> getInput() {
 	bool loop = true;
 	std::vector<ThreeDPoint> return_value;
 
-	while ( loop ) {
-		std::cout << "Enter X value: ";
-		std::cin >> input_x;
-		std::cout << "Enter Y value: ";
-		std::cin >> input_y;
-		std::cout << "Enter Z value: ";
-		std::cin >> input_z;
+	std::ifstream input_file( "input2.txt" );
+
+
+	while (    input_file >> input_x
+			&& input_file >> input_y
+			&& input_file >> input_z ) {
 
 		ThreeDPoint p;
 		p.x = input_x;
 		p.y = input_y;
 		p.z = input_z;
 		return_value.push_back( p );
-
-		char response;
-		std::cout << "Would you like to enter another pair? (y/n): ";
-		std::cin >> response;
-		if ( response != 'y' ) {
-			loop = false;
-		}
 	}
 
 	return return_value;
@@ -77,10 +69,10 @@ std::vector<ThreeDPoint> getInput() {
 
 void trainModel( std::vector <ThreeDPoint> input, double& b0, double& b1, double& b2, double learning_rate ) {
 	int training_loops = 1;
-	std::cout << "How many training loops? ";
 	std::cin >> training_loops;
 
 	std::cout << "Training Model" << std::endl;
+	std::cout << "Training Loops: " << training_loops << std::endl;
 	std::cout << "Learning Rate: " << learning_rate << std::endl;
 
 	for ( int i = 0; i < training_loops; ++i ) {
@@ -88,7 +80,7 @@ void trainModel( std::vector <ThreeDPoint> input, double& b0, double& b1, double
 			double predicted_value = b0 + b1 * point.x + b2 * point.y;
 			double error_value = predicted_value - point.z;
 			std::cout << "b0: " << b0 << ", b1: " << b1 << ", b2: " << b2 << std::endl;
-			std::cout << "g(" << point.x << ", " << point.y << ") = " << predicted_value << " | Error: " << error_value << std::endl;
+			std::cout << "g(" << point.x << ", " << point.y << ") ~= " << predicted_value << " | = " << point.z << " | Error: " << error_value << std::endl;
 			b0 = b0 - ( learning_rate * error_value );
 			b1 = b1 - ( learning_rate * error_value * point.x );
 			b2 = b2 - ( learning_rate * error_value * point.y );
